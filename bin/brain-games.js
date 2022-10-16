15f1f85e85cab09daa6fd82e7e56f450638b5bd9
nudaso <nudasoaleksej@gmail.com>
2022-10-16 15:58:15 +0300

    create module cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #! /usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import {hello, yourName} from './../src/cli.js';
+hello();
+yourName();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readLineSync from 'readline-sync';
+export const hello = () => {
+    console.log('Welcome to the Brain Games!');
+};
+
+export const yourName = () => {
+    const name = readLineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}`);
+};
