bc4c3c186d49d9fb697ccd996ab1fd023d1a122e
nudaso <nudasoaleksej@gmail.com>
2022-11-25 23:27:43 +0300

    step 7

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import { startGame, rnd } from '../../src/index.js';
+const gcd = (a, b) => {
+  if (a === b) {
+    return a;
+  }
+  return a > b ? gcd(a-b, b) : gcd(a, b-a);
+}
+
+const arr = [];
+const essenceMassage = "Find the greatest common divisor of given numbers.";
+
+for (let i = 0; i < 3; i += 1) {
+  const num1 = rnd(100);
+  const num2 = rnd(100);
+  const question = `${num1} ${num2}`;
+  const answer = `${gcd(num1, num2)}`;
+  arr.push([question, answer]);
+}
+
+startGame(arr, essenceMassage);
