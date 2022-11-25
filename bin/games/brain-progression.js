6a8750e0400dcd3a0a7e407ea807cc683b4ac806
nudaso <nudasoaleksej@gmail.com>
2022-11-25 23:59:29 +0300

    step 8

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import { startGame, rnd } from '../../src/index.js';
+
+const arr = [];
+const essenceMassage = "What number is missing in the progression?";
+const indexArr = [];
+const createIndex = (n) => {
+  const index = rnd(n-1);
+  if (indexArr.includes(index)) {
+    return createIndex(n);
+  }
+  return index;
+};
+for (let i = 0; i < 3; i += 1) {
+  const start = rnd(30);
+  const step = rnd(10, 1);
+  const n = rnd(10, 7);
+  const index = createIndex(n);
+  const answer = `${start + step* index}`;
+  const question = [];
+  for (let j = 0; j < n; j += 1) {
+    if (j === index) {
+      question.push("..");
+      continue;
+    }
+    question.push(start + step*j);
+  }
+  arr.push([question.join(' '), answer]);
+}
+
+startGame(arr, essenceMassage);
